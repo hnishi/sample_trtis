72f85c54c7d12f76790d76f84c5794c23155ff42
kurama554101 <shotasakamoto554101@gmail.com>
2020-01-06 23:17:14 +0900

    tmp commit

---
+++ src/custom_backend/mecab_model/mecab_model.cc
@@ -0,0 +1,78 @@
+#include <chrono>
+#include <string>
+#include <thread>
+#include <map>
+#include <mecab.h>
+
+#include "src/core/model_config.h"
+#include "src/core/model_config.pb.h"
+#include "src/custom/sdk/custom_instance.h"
+
+#define LOG_ERROR std::cerr
+#define LOG_INFO std::cout
+
+namespace nvidia { namespace inferenceserver { namespace custom {
+namespace mecab_model {
+
+// Context object. All state must be kept in this object.
+class Context : public CustomInstance {
+ public:
+  Context(
+      const std::string& instance_name, const ModelConfig& config,
+      const int gpu_device);
+  ~Context();
+
+  int Init();
+
+  int Execute(
+      const uint32_t payload_cnt, CustomPayload* payloads,
+      CustomGetNextInputFn_t input_fn, CustomGetOutputFn_t output_fn);
+
+ private:
+};
+
+Context::Context(
+    const std::string& instance_name, const ModelConfig& model_config,
+    const int gpu_device)
+    : CustomInstance(instance_name, model_config, gpu_device)
+{
+}
+
+Context::~Context() {}
+
+int
+Context::Init()
+{
+  return ErrorCodes::Success;
+}
+
+int
+Context::Execute(
+    const uint32_t payload_cnt, CustomPayload* payloads,
+    CustomGetNextInputFn_t input_fn, CustomGetOutputFn_t output_fn)
+{
+    return ErrorCodes::Success;
+}
+
+}  // namespace mecab_model
+
+// Creates a new mecab_model context instance
+int
+CustomInstance::Create(
+    CustomInstance** instance, const std::string& name,
+    const ModelConfig& model_config, int gpu_device,
+    const CustomInitializeData* data)
+{
+  mecab_model::Context* context =
+      new mecab_model::Context(name, model_config, gpu_device);
+
+  *instance = context;
+
+  if (context == nullptr) {
+    return ErrorCodes::CreationFailure;
+  }
+
+  return context->Init();
+}
+
+}}}  // namespace nvidia::inferenceserver::custom
